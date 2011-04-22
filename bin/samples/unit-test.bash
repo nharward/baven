fb192bc32416141369ea3bc2700adf802b5eae3d
Nathaniel Harward <nharward@gmail.com>
2011-04-22 01:36:53 -0700

    Added MD5/SHA1 sums for unit-test-0.1.0.  Also made a "samples" directory in bin/ and moved unit-test.bash there.

--- bin/samples/unit-test.bash
+++ bin/samples/unit-test.bash
@@ -1,9 +1,9 @@
-#! /bin/bash -x
+#! /bin/bash
 
-BAVEN_DEBUG=yes
-BAVEN_VERBOSE=yes
+#BAVEN_DEBUG=yes
+#BAVEN_VERBOSE=yes
 
-source $(dirname "${0}")/baven.bash
+source $(dirname "${0}")/../baven.bash
 $(bvn.load_plugin baven unit-test 0.1.0)
 
 bvn.is_plugin_loaded baven unit-test 0.1.0

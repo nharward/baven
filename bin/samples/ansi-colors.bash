a5d4eadcca8cd956fa0a42372e6cefbae4116700
Nathaniel Harward <nharward@gmail.com>
2011-04-25 23:35:50 -0700

    Updating ansi-color to use standard naming convention

--- bin/samples/ansi-colors.bash
+++ bin/samples/ansi-colors.bash
@@ -8,17 +8,17 @@ $(bvn.load_plugin baven ansi-color 1.0.0)
 
 if bvn.is_plugin_loaded baven ansi-color 1.0.0; then
     cat <<__COLOR_DEMO__
-Standout  : $(color.standout "Hello, world!")
-Underline : $(color.underline "Hello, world!")
-Dim       : $(color.dim "Hello, world!")
-Black     : $(color.black "Hello, world!")
-Red       : $(color.red "Hello, world!")
-Green     : $(color.green "Hello, world!")
-Yellow    : $(color.yellow "Hello, world!")
-Blue      : $(color.blue "Hello, world!")
-Magenta   : $(color.magenta "Hello, world!")
-Cyan      : $(color.cyan "Hello, world!")
-White     : $(color.white "Hello, world!")
+Standout  : $(ansi-color.standout "Hello, world!")
+Underline : $(ansi-color.underline "Hello, world!")
+Dim       : $(ansi-color.dim "Hello, world!")
+Black     : $(ansi-color.black "Hello, world!")
+Red       : $(ansi-color.red "Hello, world!")
+Green     : $(ansi-color.green "Hello, world!")
+Yellow    : $(ansi-color.yellow "Hello, world!")
+Blue      : $(ansi-color.blue "Hello, world!")
+Magenta   : $(ansi-color.magenta "Hello, world!")
+Cyan      : $(ansi-color.cyan "Hello, world!")
+White     : $(ansi-color.white "Hello, world!")
 __COLOR_DEMO__
 else
     bvn.err "Could not load ANSI color plugin"
